c4487f9a36cceefbebc906cd722b6be0eed2d85f
Mark Rudolph <mark@k8ty.app>
2024-12-31 23:11:22 -0500

    coming together

--- src/commands/build/SiteBuilder.scala
+++ src/commands/build/SiteBuilder.scala
@@ -2,7 +2,10 @@ package commands.build
 
 import dev.wishingtree.branch.macaroni.fs.PathOps.*
 import org.commonmark.Extension
-import org.commonmark.ext.front.matter.YamlFrontMatterExtension
+import org.commonmark.ext.front.matter.{
+  YamlFrontMatterExtension,
+  YamlFrontMatterVisitor
+}
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
 import org.jsoup.Jsoup
@@ -39,49 +42,86 @@ object SiteBuilder {
       HtmlRenderer.builder().build()
 
     override def copyStatic(): Unit = Try {
+      val _thisBuild = root.getParent / "build"
       Files
         .walk(root / "static")
         .sorted(Comparator.naturalOrder())
         .forEach { path =>
           if Files.isDirectory(path) then
             Files.createDirectories(
-              root.getParent / "build" / path.relativeTo(root / "static")
+              _thisBuild / path.relativeTo(root / "static")
             )
           else
             Files.copy(
               path,
-              root.getParent / "build" / path.relativeTo(root / "static")
+              _thisBuild / path.relativeTo(root / "static")
             )
         }
     }
 
     private def buildPages(root: Path): Unit = {
-      val _thisRoot = root / "site" / "pages"
+      val _thisRoot  = root / "site" / "pages"
+      val _thisBuild = root.getParent / "build"
       Files
         .walk(_thisRoot)
+        .filter(p => p.toString.endsWith(".md") || Files.isDirectory(p))
         .sorted(Comparator.naturalOrder())
         .forEach { path =>
           if Files.isDirectory(path) then
-            println(path.relativeTo(_thisRoot))
             Files.createDirectories(
-              root.getParent / "build" / path.relativeTo(_thisRoot)
+              _thisBuild / path.relativeTo(_thisRoot)
             )
           else
-            println(path.relativeTo(_thisRoot))
+            val siteTemplate = ContentLoader(root).loadSiteTemplate().get
             val pageTemplate = ContentLoader(root).loadPageTemplate().get
+
+            val siteHtml = Jsoup.parse(siteTemplate)
             val pageHtml = Jsoup.parse(pageTemplate)
-            val pageContent = ContentLoader(root).load(path).get
-            val content = pageHtml.getElementById("page-content").html(htmlRenderer.render(mdParser.parse(pageContent)))
-            val fn = path.relativeTo(path.getParent).toString.stripSuffix(".md") + ".html"
+
+            val rawContent    = ContentLoader(root).load(path).get
+            val parsedContent = mdParser.parse(rawContent)
+
+            val visitor     = new YamlFrontMatterVisitor()
+            parsedContent.accept(visitor)
+            val frontMatter = visitor.getData.asScala.toMap
+
+            pageHtml
+              .getElementById("page-content")
+              .html(htmlRenderer.render(parsedContent))
+
+            siteHtml
+              .getElementById("site-content")
+              .html(pageHtml.html())
+
+            frontMatter.get("title").foreach { l =>
+              l.asScala.headOption.foreach { title =>
+                siteHtml.title(title)
+              }
+            }
+
+            val fn = path
+              .relativeTo(path.getParent)
+              .toString
+              .stripSuffix(".md") + ".html"
+
+            val destination = path.relativeTo(_thisRoot).getNameCount match {
+              case 1 => _thisBuild / fn
+              case _ => _thisBuild / path.relativeTo(_thisRoot).getParent / fn
+            }
+
             Files.writeString(
-              root.getParent / "build" / path.relativeTo(_thisRoot) / path.getParent / fn,
-              pageHtml.html()
+              destination,
+              siteHtml.html()
             )
         }
     }
 
+    private def buildBlog(root: Path): Unit =
+      ()
+
     override def parseSite(): Unit = {
       buildPages(root)
+      buildBlog(root)
     }
 
     override def cleanBuild(): Unit =
