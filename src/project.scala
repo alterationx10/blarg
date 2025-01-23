af8a6a357b34f9bf70ab0df1cce5c08d69366187
Mark Rudolph <mark@k8ty.app>
2025-01-23 22:42:47 +0000

    Update dependencies

--- src/project.scala
+++ src/project.scala
@@ -5,7 +5,7 @@
 //> using  options -no-indent -rewrite -source:3.4-migration
 
 //> using resourceDir ../new_site
-//> using test.dep org.scalameta::munit:1.0.4
+//> using test.dep org.scalameta::munit:1.1.0
 
 //> using dep org.commonmark:commonmark:0.24.0
 //> using dep org.commonmark:commonmark-ext-autolink:0.24.0
