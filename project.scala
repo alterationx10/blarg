05aa7d7fe34955baeb560349b908af31e7ad1ba1
Mark Rudolph <mark@k8ty.app>
2025-10-14 22:53:41 -0400

    bump deps

--- project.scala
+++ project.scala
@@ -1,18 +1,18 @@
 //> using scala 3
 //> using jvm 23
-//> using dep dev.alteration::branch:0.0.0-SNAPSHOT
+//> using dep dev.alteration::branch:0.0.12
 
 //> using  options -no-indent -rewrite -source:3.4-migration
 
 //> using resourceDir ../new_site
-//> using test.dep org.scalameta::munit:1.2.0
+//> using test.dep org.scalameta::munit:1.2.1
 
-//> using dep org.commonmark:commonmark:0.26.0
-//> using dep org.commonmark:commonmark-ext-autolink:0.26.0
-//> using dep org.commonmark:commonmark-ext-gfm-strikethrough:0.26.0
-//> using dep org.commonmark:commonmark-ext-gfm-tables:0.26.0
-//> using dep org.commonmark:commonmark-ext-footnotes:0.26.0
-//> using dep org.commonmark:commonmark-ext-heading-anchor:0.26.0
-//> using dep org.commonmark:commonmark-ext-ins:0.26.0
-//> using dep org.commonmark:commonmark-ext-yaml-front-matter:0.26.0
-//> using dep org.commonmark:commonmark-ext-image-attributes:0.26.0
+//> using dep org.commonmark:commonmark:0.27.0
+//> using dep org.commonmark:commonmark-ext-autolink:0.27.0
+//> using dep org.commonmark:commonmark-ext-gfm-strikethrough:0.27.0
+//> using dep org.commonmark:commonmark-ext-gfm-tables:0.27.0
+//> using dep org.commonmark:commonmark-ext-footnotes:0.27.0
+//> using dep org.commonmark:commonmark-ext-heading-anchor:0.27.0
+//> using dep org.commonmark:commonmark-ext-ins:0.27.0
+//> using dep org.commonmark:commonmark-ext-yaml-front-matter:0.27.0
+//> using dep org.commonmark:commonmark-ext-image-attributes:0.27.0
